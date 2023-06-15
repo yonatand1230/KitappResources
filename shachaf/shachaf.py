a6c5fb329007677d1b33516faee6fa858b9bf821
Yonatan Daga <36605190+yonatand1230@users.noreply.github.com>
2023-06-16 01:50:08 +0300

    Make a Shachaf class

--- shachaf/shachaf.py
+++ shachaf/shachaf.py
@@ -36,53 +36,54 @@ def xstrip(s, tag, cla):
     td_tag = soup.find(tag, {'class':cla})
     return td_tag.get_text()
 
-### GET HOLIDAYS AS DICT: ###
-def get_holidays(html):
-    holidays = {}
+class Shachaf:
+    ### GET HOLIDAYS AS DICT: ###
+    def get_holidays(html):
+        holidays = {}
 
-    page = bs4.BeautifulSoup(html, features="lxml")
-    l = page.find('table', class_='TTTable').find_all('td', class_='CTitle')
+        page = bs4.BeautifulSoup(html, features="lxml")
+        l = page.find('table', class_='TTTable').find_all('td', class_='CTitle')
 
-    for i in l:
-        current_page = bs4.BeautifulSoup(str(i), features='lxml')
-        if current_page.find('span', {'class':'Holiday'}) != None:
-            # save holiday name
-            holiday_name = current_page.find('span', {'class':'Holiday'}).get_text()
-            
-            # save holiday date
-            date = current_page.find('span').get_text().replace('   ','') # we get a string like "sunday 01.02"
-            date_num = get_numbers(date) # we extract the numbers and get: "01.02"        
-            
-            holidays.update({date_num:holiday_name})
-    return holidays
+        for i in l:
+            current_page = bs4.BeautifulSoup(str(i), features='lxml')
+            if current_page.find('span', {'class':'Holiday'}) != None:
+                # save holiday name
+                holiday_name = current_page.find('span', {'class':'Holiday'}).get_text()
+                
+                # save holiday date
+                date = current_page.find('span').get_text().replace('   ','') # we get a string like "sunday 01.02"
+                date_num = get_numbers(date) # we extract the numbers and get: "01.02"        
+                
+                holidays.update({date_num:holiday_name})
+        return holidays
 
-### GET CHANGES IN SCHEDULE ###
-def get_changes(html):
-    all_changes = []
-    page = bs4.BeautifulSoup(html, features="lxml")
-    hours = page.find_all('tr', {'bgcolor':"#ffffff"}) # get list of all hours
-    hours.pop()
+    ### GET CHANGES IN SCHEDULE ###
+    def get_changes(html):
+        all_changes = []
+        page = bs4.BeautifulSoup(html, features="lxml")
+        hours = page.find_all('tr', {'bgcolor':"#ffffff"}) # get list of all hours
+        hours.pop()
 
-    for hour in hours: # loop through hours
-        h_text = hour.find('span', {'class':'hour-time'})
-        if h_text != None:
-            hour_text = h_text.get_text()
-        cells = hour.find_all('td', {'class':'TTCell'}) # cells for current hour
-        #print(cells)
-        for c in cells:
-            tr = c.find_all('tr')
-            if len(tr) != 0:
-                if len(tr) == 1:
-                    finalChange = tr[0].find('td').get_text(separator='\n') # CHANGE - string
-                elif len(tr)>1:
-                    finalChange=[]
-                    for i in tr:
-                        one_change = i.find('td').get_text(separator='\n') # only one change - string
-                        finalChange.append(one_change)
-                finalHour = hour_text # HOUR
-                finalDay = cells.index(c) # DAY
-                myChange = {'day':finalDay, 'hour':finalHour, 'body':finalChange}
-                all_changes.append(myChange)
-            
-    return all_changes
+        for hour in hours: # loop through hours
+            h_text = hour.find('span', {'class':'hour-time'})
+            if h_text != None:
+                hour_text = h_text.get_text()
+            cells = hour.find_all('td', {'class':'TTCell'}) # cells for current hour
+            #print(cells)
+            for c in cells:
+                tr = c.find_all('tr')
+                if len(tr) != 0:
+                    if len(tr) == 1:
+                        finalChange = tr[0].find('td').get_text(separator='\n') # CHANGE - string
+                    elif len(tr)>1:
+                        finalChange=[]
+                        for i in tr:
+                            one_change = i.find('td').get_text(separator='\n') # only one change - string
+                            finalChange.append(one_change)
+                    finalHour = hour_text # HOUR
+                    finalDay = cells.index(c) # DAY
+                    myChange = {'day':finalDay, 'hour':finalHour, 'body':finalChange}
+                    all_changes.append(myChange)
+                
+        return all_changes
 
